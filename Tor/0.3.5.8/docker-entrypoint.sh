2bc8ab438848285d952a0722abae9035c4512bb9
nicolas.dorier <nicolas.dorier@gmail.com>
2019-03-10 22:32:03 +0900

    [TOR] remove cookieauthentication, add TOR_PASSWORD

--- Tor/0.3.5.8/docker-entrypoint.sh
+++ Tor/0.3.5.8/docker-entrypoint.sh
@@ -6,9 +6,14 @@ mkdir -p "$TOR_DATA"
 cat <<-EOF > "$TOR_CONFIG"
 ControlPort 0.0.0.0:9051
 SOCKSPort 0.0.0.0:9050
-CookieAuthentication 1
 ${TOR_EXTRA_ARGS}
 EOF
+
+if [[ "$TOR_PASSWORD" ]]; then
+    TOR_PASSWORD_HASH="$(gosu tor tor --hash-password "$TOR_PASSWORD")"
+    echo "HashedControlPassword $TOR_PASSWORD_HASH" >> "$TOR_CONFIG"
+    echo "'HashedControlPassword $TOR_PASSWORD_HASH' added to tor config"
+fi
 
 chown tor:tor "$TOR_CONFIG"
 chown -R tor "$TOR_DATA"
