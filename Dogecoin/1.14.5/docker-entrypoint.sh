4c14ec5efb2acfe1b2ec44cebf1c73c12231d0c2
nicolas.dorier <nicolas.dorier@gmail.com>
2022-06-22 16:55:49 +0900

    Fix dogecoin regtest and testnet support

--- Dogecoin/1.14.5/docker-entrypoint.sh
+++ Dogecoin/1.14.5/docker-entrypoint.sh
@@ -4,7 +4,19 @@ set -e
 if [[ "$1" == "dogecoin-cli" || "$1" == "dogecoin-tx" || "$1" == "dogecoind" || "$1" == "test_dogecoin" ]]; then
 	mkdir -p "$DOGECOIN_DATA"
 
+	CONFIG_PREFIX=""
+    if [[ "${BITCOIN_NETWORK}" == "regtest" ]]; then
+        CONFIG_PREFIX=$'regtest=1\n'
+    fi
+    if [[ "${BITCOIN_NETWORK}" == "testnet" ]]; then
+        CONFIG_PREFIX=$'testnet=1\n'
+    fi
+    if [[ "${BITCOIN_NETWORK}" == "mainnet" ]]; then
+        CONFIG_PREFIX=$'mainnet=1\n'
+    fi
+
 	cat <<-EOF > "$DOGECOIN_DATA/dogecoin.conf"
+	${CONFIG_PREFIX}
 	printtoconsole=1
 	rpcallowip=::/0
 	${DOGECOIN_EXTRA_ARGS}
