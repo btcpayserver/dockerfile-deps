df09b86c6b52dc0db135988fa9e88ab7df888995
Dennis Reimann <mail@dennisreimann.de>
2022-10-29 18:18:15 +0200

    Upgrade JoinMarket to v0.9.8

---
+++ JoinMarket/0.9.8/docker-entrypoint.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+set -e
+
+cd ..
+. jmvenv/bin/activate
+cd scripts
+
+export JM_onion_serving_host="$(/sbin/ip route|awk '/src/ { print $9 }')"
+
+# First we restore the default cfg as created by wallet-tool.py generate
+if ! [ -f "$CONFIG" ]; then
+    cp "$DEFAULT_CONFIG" "$CONFIG"
+fi
+
+if ! [ -f "$AUTO_START" ]; then
+    cp "$DEFAULT_AUTO_START" "$AUTO_START"
+fi
+
+# setup basic authentication
+BASIC_AUTH_USER=${APP_USER:?APP_USER empty or unset}
+BASIC_AUTH_PASS=${APP_PASSWORD:?APP_PASSWORD empty or unset}
+mkdir -p "${DATADIR}/nginx/"
+echo -e "${BASIC_AUTH_USER}:$(openssl passwd -quiet -6 <<< echo "${BASIC_AUTH_PASS}")\n" > "${DATADIR}/nginx/.htpasswd"
+
+# generate ssl certificates for jmwalletd
+if ! [ -f "${DATADIR}/ssl/key.pem" ]; then
+    subj="/C=US/ST=Utah/L=Lehi/O=Your Company, Inc./OU=IT/CN=example.com"
+    mkdir -p "${DATADIR}/ssl/" \
+      && pushd "$_" \
+      && openssl req -newkey rsa:4096 -x509 -sha256 -days 3650 -nodes -out cert.pem -keyout key.pem -subj "$subj" \
+      && popd
+fi
+
+# ensure 'logs' directory exists
+mkdir -p "${DATADIR}/logs"
+
+# auto start services
+while read p; do
+  [[ "$p" == "" ]] && continue
+  [[ "$p" == "#"* ]] && continue
+  echo "Auto start: $p"
+  file_path="/etc/supervisor/conf.d/$p.conf"
+  if [ -f "$file_path" ]; then
+    sed -i 's/autostart=false/autostart=true/g' $file_path
+  else
+    echo "$file_path not found"
+  fi
+done <$AUTO_START
+
+# For every env variable JM_FOO=BAR, replace the default configuration value of 'foo' by 'BAR'
+while IFS='=' read -r -d '' envkey parsedval; do
+    n="${envkey,,}" # lowercase
+    if [[ "$n" =  jm_* ]]; then
+        v=${!envkey} # reread environment variable - characters might have been dropped (e.g 'ending in =')
+        n="${n:3}" # drop jm_
+        sed -i "s/^$n =.*/$n = $v/g" "$CONFIG" || echo "Couldn't set : $n = $v, please modify $CONFIG manually"
+    fi
+done < <(env -0)
+#####################################
+
+if [[ "${READY_FILE}" ]]; then
+    echo "Waiting $READY_FILE to be created..."
+    while [ ! -f "$READY_FILE" ]; do sleep 1; done
+    echo "The chain is fully synched"
+fi
+
+exec supervisord
---
+++ JoinMarket/0.9.8/linuxamd64.Dockerfile
@@ -0,0 +1,37 @@
+FROM python:3.9.7-slim-bullseye
+
+RUN apt-get update && \
+    apt-get install -qq --no-install-recommends curl tini sudo procps vim git iproute2 \
+    build-essential automake pkg-config libtool libgmp-dev libltdl-dev python3-dev virtualenv python3-pip supervisor && \
+    rm -rf /var/lib/apt/lists/*
+
+ENV REPO https://github.com/JoinMarket-Org/joinmarket-clientserver
+ENV REPO_REF v0.9.8
+
+WORKDIR /src
+RUN git clone "$REPO" . --depth=10 --branch "$REPO_REF" && git checkout "$REPO_REF"
+
+RUN ./install.sh --disable-secp-check --without-qt
+ENV DATADIR /root/.joinmarket
+ENV CONFIG ${DATADIR}/joinmarket.cfg
+ENV DEFAULT_CONFIG /root/default.cfg
+ENV DEFAULT_AUTO_START /root/autostart
+ENV AUTO_START ${DATADIR}/autostart
+ENV ENV_FILE "${DATADIR}/.env"
+RUN . jmvenv/bin/activate && cd /src/scripts && \
+    pip install matplotlib && \
+    (python wallet-tool.py generate || true) \
+    && cp "${CONFIG}" "${DEFAULT_CONFIG}"
+WORKDIR /src/scripts
+COPY docker-entrypoint.sh .
+COPY *.sh ./
+COPY autostart /root/
+COPY supervisor-conf/*.conf /etc/supervisor/conf.d/
+ENV PATH /src/scripts:$PATH
+# jmwallet daemon
+EXPOSE 28183
+# payjoin server
+EXPOSE 8080
+# obwatch
+EXPOSE 62601
+ENTRYPOINT  [ "tini", "-g", "--", "./docker-entrypoint.sh" ]
