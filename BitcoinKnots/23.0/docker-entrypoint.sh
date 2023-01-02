c94a3ecd0b2a13f1b47646c11a970c8e64bb6575
Kukks <evilkukka@gmail.com>
2023-01-02 11:06:56 +0100

    create legacy wallet in knots like in core

--- BitcoinKnots/23.0/docker-entrypoint.sh
+++ BitcoinKnots/23.0/docker-entrypoint.sh
@@ -27,7 +27,7 @@ if [[ "$1" == "bitcoin-cli" || "$1" == "bitcoin-tx" || "$1" == "bitcoind" || "$1
 		CONFIG_PREFIX="${CONFIG_PREFIX}${NL}walletdir=${WALLETDIR}${NL}"
 		if ! [[ -f "${WALLETFILE}" ]]; then
 		  echo "The wallet does not exists, creating it at ${WALLETDIR}..."
-		  gosu bitcoin bitcoin-wallet "-datadir=${WALLETDIR}" "-wallet=" create
+		  gosu bitcoin bitcoin-wallet "-datadir=${WALLETDIR}" "-legacy" "-wallet=" create
 		fi
 	fi
 
@@ -58,7 +58,7 @@ if [[ "$1" == "bitcoin-cli" || "$1" == "bitcoin-tx" || "$1" == "bitcoind" || "$1
 	chown -R bitcoin "$BITCOIN_DATA"
 	ln -sfn "$BITCOIN_DATA" /home/bitcoin/.bitcoin
 	chown -h bitcoin:bitcoin /home/bitcoin/.bitcoin
-
+	rm -f /home/bitcoin/.bitcoin/settings.json
 	exec gosu bitcoin "$@"
 else
 	exec "$@"
