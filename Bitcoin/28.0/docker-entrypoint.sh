fe439004aa3b17012a5e8f7c998d92a246015f79
wario_is_here <117512622+warioishere@users.noreply.github.com>
2025-01-11 10:39:16 +0100

    Update docker-entrypoint.sh
    correction removing corrupted peers.dat script

--- Bitcoin/28.0/docker-entrypoint.sh
+++ Bitcoin/28.0/docker-entrypoint.sh
@@ -53,6 +53,14 @@ if [[ "$1" == "bitcoin-cli" || "$1" == "bitcoin-tx" || "$1" == "bitcoind" || "$1
 		fi
 	fi
 
+ 	# ensure correct ownership and linking of data directory
+	# we do not update group ownership here, in case users want to mount
+	# a host directory and still retain access to it
+	chown -R bitcoin "$BITCOIN_DATA"
+	ln -sfn "$BITCOIN_DATA" /home/bitcoin/.bitcoin
+	chown -h bitcoin:bitcoin /home/bitcoin/.bitcoin
+	rm -f /home/bitcoin/.bitcoin/settings.json
+
 	exec gosu bitcoin "$@"
 else
 	exec "$@"
